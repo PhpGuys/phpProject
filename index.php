6157ef9f86648e7f80cfb7e87e10b798ed2be917
ksidyagin <64638744+ksidyagin@users.noreply.github.com>
2020-10-19 20:36:06 +0300

    new

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    
+    <link rel="stylesheet" href="style/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+    <h3>Php Top!</h3>
 <?php
 echo "Hello PHP!";
 ?>
