eb02ea985dd66276fc651525d22c8d17cf76d201
doki44 <62612953+doki44@users.noreply.github.com>
2020-10-21 20:12:11 +0300

    z

--- footer.php
+++ footer.php
@@ -33,10 +33,10 @@
         <div class="col-md-2 footer-social animated fadeInDown">
           <h4>Follow Us</h4>
           <ul>
-          <li><a href="#">Facebook</a></li>
-          <li><a href="#">Twitter</a></li>
-          <li><a href="#">Instagram</a></li>
-          <li><a href="#">VK</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">VK</a></li>
           </ul>
         </div>
       <div class="col-md-3 footer-ns animated fadeInRight">
