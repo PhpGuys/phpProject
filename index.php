83fc93958d1cc6cf34b0ae81b17730d80a960ff4
doki44 <62612953+doki44@users.noreply.github.com>
2020-10-21 20:00:07 +0300

    ))))))))

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'header.php';
 <section class="welcome">
     <div class="welcome__title">Добро пожаловать на форум!</div>
     <div class="welcome__descr">
-    Логанов---гений!!!!
+    Владимир Путин - молодец!!!!!!!!!! Политик, лидер и борец.
     </div>
     <button onclick="document.location= 'registr.php'" class="welcome__reg">Зарегистироваться</button>
 </section>
