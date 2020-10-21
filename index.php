8f9e693ae2f66aa7fe02afd8175ee2c6f494d7aa
Artoum2000 <62651092+Artoum2000@users.noreply.github.com>
2020-10-21 19:56:51 +0300

    логанов

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'header.php';
 <section class="welcome">
     <div class="welcome__title">Добро пожаловать на форум!</div>
     <div class="welcome__descr">
-    Здесь вы получите ответы на интересующие вас вопросы
+    Логанов---гений!!!!
     </div>
     <button onclick="document.location= 'registr.php'" class="welcome__reg">Зарегистироваться</button>
 </section>
