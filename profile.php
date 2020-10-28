567898de41036a70b8d82337b572fc5a7781e06d
doki44 <62612953+doki44@users.noreply.github.com>
2020-10-28 11:41:37 +0300

    profile

---
+++ profile.php
@@ -0,0 +1,107 @@
+<link rel="stylesheet" href="https://bootstraptema.ru/plugins/2015/bootstrap3/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+
+<br><br><br>
+
+<style>
+body{background:url(https://bootstraptema.ru/images/bg/bg-1.png)}
+
+#main {
+ background-color: #f2f2f2;
+ padding: 20px;
+-webkit-border-radius: 4px;
+ -moz-border-radius: 4px;
+ -ms-border-radius: 4px;
+ -o-border-radius: 4px;
+ border-radius: 4px;
+ border-bottom: 4px solid #ddd;
+}
+#real-estates-detail #author img {
+ -webkit-border-radius: 100%;
+ -moz-border-radius: 100%;
+ -ms-border-radius: 100%;
+ -o-border-radius: 100%;
+ border-radius: 100%;
+ border: 5px solid #ecf0f1;
+ margin-bottom: 10px;
+}
+#real-estates-detail .sosmed-author i.fa {
+ width: 30px;
+ height: 30px;
+ border: 2px solid #bdc3c7;
+ color: #bdc3c7;
+ padding-top: 6px;
+ margin-top: 10px;
+}
+.panel-default .panel-heading {
+ background-color: #fff;
+}
+#real-estates-detail .slides li img {
+ height: 450px;
+}
+</style>
+
+<div class="container">
+<div id="main">
+
+
+ <div class="row" id="real-estates-detail">
+ <div class="col-lg-4 col-md-4 col-xs-12">
+ <div class="panel panel-default">
+ <div class="panel-heading">
+ <header class="panel-title">
+ <div class="text-center">
+ <strong>Пользователь сайта</strong>
+ </div>
+ </header>
+ </div>
+ <div class="panel-body">
+ <div class="text-center" id="author">
+ <img src="ref">
+ <h3>Name</h3>
+ <small class="label label-warning">Country</small>
+ <p>Description</p>
+ <p class="sosmed-author">
+ <a href="#"><i class="fa fa-facebook" title="Contacts"></i></a>
+ <a href="#"><i class="fa fa-twitter" title="Contacts"></i></a>
+ <a href="#"><i class="fa fa-google-plus" title="Contacts"></i></a>
+ <a href="#"><i class="fa fa-linkedin" title="Contacts"></i></a>
+ </p>
+ </div>
+ </div>
+ </div>
+ </div>
+ <div class="col-lg-8 col-md-8 col-xs-12">
+ <div class="panel">
+ <div class="panel-body">
+ <ul id="myTab" class="nav nav-pills">
+ <li class="active"><a href="#detail" data-toggle="tab">О пользователе</a></li>
+ </ul>
+ <div id="myTabContent" class="tab-content">
+<hr>
+ <div class="tab-pane fade active in" id="detail">
+ <h4>Ваш профиль</h4>
+ <table class="table table-th-block">
+ <tbody>
+ <tr><td class="active">Зарегистрирован:</td><td>тут тоже скрипт нужен</td></tr>
+ <tr><td class="active">Последняя активность:</td><td>может быть, ток нужен скрипт</td></tr>
+ <tr><td class="active">Страна:</td><td>Россия</td></tr>
+ <tr><td class="active">Город:</td><td>город???? хз нужен ли</td></tr>
+ <tr><td class="active">Пол:</td><td>Мужской</td></tr>
+ <tr><td class="active">Возраст:</td><td>возраст</td></tr>
+ <tr><td class="active">Почта:</td><td>e-mail</td></tr>
+ <tr><td class="active">Репутация пользователя:</td><td>Хз надо или нет</td></tr>
+ 
+</tbody>
+ </table>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+</div>
+
+</div><!-- /.main -->
+<!-- /.container -->
